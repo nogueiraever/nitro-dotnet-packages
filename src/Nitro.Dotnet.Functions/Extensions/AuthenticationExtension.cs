12ee88cf20386168e3be145f688dd037e346041a
Everton Nogueira <ehve.tsi@gmail.com>
2022-08-04 20:40:18 -0300

    First Commit

---
+++ src/Nitro.Dotnet.Functions/Extensions/AuthenticationExtension.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Nitro.Dotnet.Functions.Extensions
+{
+    public static class AuthenticationExtension
+    {
+        public static void RegisterAuthentication(this IServiceCollection services, IConfiguration configuration)
+        {
+
+            services.AddAuthentication(o =>
+            {
+                o.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                o.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                o.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(o =>
+            {
+                o.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(configuration["JwtTokenSettingsIssuerSigningKey"])),
+                    ValidateIssuer = true,
+                    ValidIssuer = configuration["JwtTokenSettingsIssuer"],
+                    ValidateLifetime = false,
+                    ValidateAudience = false,
+                    ClockSkew = TimeSpan.Zero,
+                };
+            });
+
+            services.AddAuthorization(auth =>
+            {
+                auth.AddPolicy("Bearer", new AuthorizationPolicyBuilder()
+
+                   .AddAuthenticationSchemes(JwtBearerDefaults.AuthenticationScheme)
+                   .RequireAuthenticatedUser()
+                   .Build());
+            });
+
+        }
+
+    }
+}
---
+++ src/Nitro.Dotnet.Functions/Response/FunctionResponse.cs
@@ -0,0 +1,16 @@
+﻿using Amazon.Lambda.APIGatewayEvents;
+using System.Net;
+using System.Text.Json;
+
+namespace Nitro.Dotnet.Functions.Response
+{
+    public class FunctionResponse : APIGatewayProxyResponse
+    {
+        public FunctionResponse(object data, HttpStatusCode httpStatusCode)
+        {
+            Body = JsonSerializer.Serialize(data);
+            StatusCode = (int)httpStatusCode;
+            Headers = new Dictionary<string, string> { { "Content-Type", "application/json" } };
+        }
+    }
+}
